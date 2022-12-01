07730f932f48aa015cb132b9b745e5b737d02671
andres <andres@192.168.1.15>
2022-12-01 10:04:37 -0500

    added local sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate --noinput
-python manage.py createsuperuser_db --username andres --password andres --noinput --email 'blank@email.com'
 python manage.py runserver 0.0.0.0:8000
