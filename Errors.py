d0c5bd9c61cf5daa03f0f769b58e90c921d0a07b
andres mejia jimenez <andres0820m@gmail.com>
2022-12-14 09:26:04 -0500

    Revert "added app errors handle (#2)"
    This reverts commit f13e54bfb00d32cec5184a4710994019f33cd8fd.

--- Errors.py
+++ Errors.py
@@ -28,16 +28,10 @@ class WrongDataOrAccountAlreadySubscribe(Exception): pass
 class OrderAsPaidError(Exception): pass
 
 
+class NequiAccountError(Exception): pass
+
+
+class BancolombiaError(Exception): pass
+
+
 class ApiConnectionError(Exception): pass
-
-
-class WrongAccountData(Exception): pass
-
-
-class BankColombiaIsDown(Exception): pass
-
-
-class AccountCantHandleTheMoney(Exception): pass
-
-
-class ConnectionError(Exception): pass
