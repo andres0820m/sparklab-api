5faedae32128ed43616534f060053421d14e15fe
andres <andres@192.168.1.16>
2022-12-17 13:20:37 -0500

    fixed old orders creation

--- binance_listener.py
+++ binance_listener.py
@@ -1,10 +1,12 @@
+import datetime
 import json
 
 from colorama import Fore
 from termcolor import colored
 
 from binance_wrapped import BinanceInfoGetter
-from constants import MAPPED_BANKS_FOR_API, MAPPED_ORDER_KEY, ORDER_TEMPLATE, AUT_USER
+from constants import MAPPED_BANKS_FOR_API, MAPPED_ORDER_KEY, ORDER_TEMPLATE, AUT_USER, \
+    MAX_TIME_TO_CREATED_ORDER_SECONDS
 from utils import mapped_dict_from_data
 from tools import str_only_numbers
 
@@ -28,43 +30,46 @@ class BinanceListener(BinanceInfoGetter):
         print(Fore.GREEN + str(status))
         if status == 404:
             order = self.get_order_info(binance_id=binance_id)['data']
-            acc_dict = ORDER_TEMPLATE.copy()
-            acc_dict['binance_id'] = str(order['orderNumber'])
-            bank = MAPPED_BANKS_FOR_API[order['payType']]
-            order_bank = self.order_wrapped.get_bank(bank='bancolombia')
-            acc_dict['bank'] = order_bank
-            acc_dict['amount'] = str(int(float(order['totalPrice'])))
-            acc_dict['usdt_price'] = order['price']
-            acc_dict['pay_id'] = order['payMethods'][0]['id']
-            mapped_dict_from_data(acc_dict=acc_dict, data=order['payMethods'][0]['fields'], bank=bank)
-            if acc_dict['id_number']:
-                order_document = self.order_wrapped.get_document(
-                    document=self.__get_type_of_document(acc_dict['id_number']))
-            else:
-                order_document = self.order_wrapped.get_document('cc')
-            acc_dict['document_type'] = order_document
-            try:
-                order_account_type = self.order_wrapped.get_account(account=acc_dict['account_type'])
-            except:
-                order_account_type = self.order_wrapped.get_account(account='Ahorros')
-            acc_dict['account_type'] = order_account_type
-            order_data = dict((MAPPED_ORDER_KEY[key], value) for (key, value) in acc_dict.items())
-            user = self.order_wrapped.get_user()
-            order_data['user'] = user
-            order_data = self.check_accounts_data(order_data)
-            response = self.order_wrapped.create_order(order_data)
-            if response.status_code > 201:
-                self.telegram_bot.send_message(chat_id=AUT_USER,
-                                               text="order {} could not be created !!".format(order_data['binance_id']))
-            else:
-                if order_data['status'] == 'created':
-                    self.telegram_bot.send_message(chat_id=AUT_USER,
-                                                   text="order {} is running :3".format(order_data['binance_id']))
+            order_date = order['createTime']
+            order_date = datetime.datetime.fromtimestamp(order_date / 1000)
+            if (datetime.datetime.now() - order_date).total_seconds() < MAX_TIME_TO_CREATED_ORDER_SECONDS:
+                acc_dict = ORDER_TEMPLATE.copy()
+                acc_dict['binance_id'] = str(order['orderNumber'])
+                bank = MAPPED_BANKS_FOR_API[order['payType']]
+                order_bank = self.order_wrapped.get_bank(bank='bancolombia')
+                acc_dict['bank'] = order_bank
+                acc_dict['amount'] = str(int(float(order['totalPrice'])))
+                acc_dict['usdt_price'] = order['price']
+                acc_dict['pay_id'] = order['payMethods'][0]['id']
+                mapped_dict_from_data(acc_dict=acc_dict, data=order['payMethods'][0]['fields'], bank=bank)
+                if acc_dict['id_number']:
+                    order_document = self.order_wrapped.get_document(
+                        document=self.__get_type_of_document(acc_dict['id_number']))
                 else:
+                    order_document = self.order_wrapped.get_document('cc')
+                acc_dict['document_type'] = order_document
+                try:
+                    order_account_type = self.order_wrapped.get_account(account=acc_dict['account_type'])
+                except:
+                    order_account_type = self.order_wrapped.get_account(account='Ahorros')
+                acc_dict['account_type'] = order_account_type
+                order_data = dict((MAPPED_ORDER_KEY[key], value) for (key, value) in acc_dict.items())
+                user = self.order_wrapped.get_user()
+                order_data['user'] = user
+                order_data = self.check_accounts_data(order_data)
+                response = self.order_wrapped.create_order(order_data)
+                if response.status_code > 201:
                     self.telegram_bot.send_message(chat_id=AUT_USER,
-                                                   text='the order {} was created but need revision'.format(
-                                                       order_data['binance_id']))
-            print(Fore.GREEN + "order with id:{} was created".format(binance_id))
+                                                   text="order {} could not be created !!".format(order_data['binance_id']))
+                else:
+                    if order_data['status'] == 'created':
+                        self.telegram_bot.send_message(chat_id=AUT_USER,
+                                                       text="order {} is running :3".format(order_data['binance_id']))
+                    else:
+                        self.telegram_bot.send_message(chat_id=AUT_USER,
+                                                       text='the order {} was created but need revision'.format(
+                                                           order_data['binance_id']))
+                print(Fore.GREEN + "order with id:{} was created".format(binance_id))
 
     def wws_on_error(self, ws, error):
         print(error)
